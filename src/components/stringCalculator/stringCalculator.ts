2e2e6d9d3bfc1fe2604040df07c938150b5a9418
Gowthemrazz <er.gowthemrazz@gmail.com>
2024-06-28 19:54:52 +0530

    written add method to handle any amount of numbers

--- src/components/stringCalculator/stringCalculator.ts
+++ src/components/stringCalculator/stringCalculator.ts
@@ -1,3 +1,30 @@
-export const add = (numbers: string) => {
-    return 0
+export const add = (numbers: string): number => {
+
+    // If the input string is empty, then it returns 0
+    if (!numbers) return 0;
+
+    // Default delimeters is comma or newline
+    let delimeter = /,|\n/;
+    let numberString = numbers;
+
+    // check if there is a custom delimeter
+    if (numbers.startsWith('//')) {
+        const delimiterEnd = numbers.indexOf('\n');
+        delimeter = new RegExp(numbers.substring(2, delimiterEnd))
+        numberString = numbers.substring(delimiterEnd + 1)
+    }
+
+    // split the number string by the delimeter and convert each part to a number
+    const numberArray = numberString.split(delimeter).map(Number);
+
+    // Filter out negative numbers from the array
+    const negativeNumbers = numberArray.filter(num => num < 0);
+
+    // Throw an exception if there are any negative numbers
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
+    }
+
+    // Atlast return the sum of the numbers in an array
+    return numberArray.reduce((sum, num) => sum + num, 0)
 }
