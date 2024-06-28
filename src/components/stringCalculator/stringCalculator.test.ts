914016f42363315546a8176f92bbe816f1be4487
Gowthemrazz <er.gowthemrazz@gmail.com>
2024-06-28 20:01:53 +0530

    case3: handles new lines between numbers - passed

--- src/components/stringCalculator/stringCalculator.test.ts
+++ src/components/stringCalculator/stringCalculator.test.ts
@@ -11,7 +11,3 @@ test('returns the number itself for a single number', () => {
 test('returns the sum of two numbers', () => {
     expect(add('1,5')).toBe(6);
 });
-
-test('handles new lines between numbers', () => {
-    expect(add('1\n2, 3')).toBe(6)
-})
