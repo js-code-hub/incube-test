22d78694ac01a91be3a639445c2d1a0a4232caef
Gowthemrazz <er.gowthemrazz@gmail.com>
2024-06-28 19:17:25 +0530

    written initial test cases

---
+++ src/components/stringCalculator/stringCalculator.test.ts
@@ -0,0 +1,13 @@
+import { add } from "./stringCalculator";
+
+test('returns 0 for an empty string', () => {
+    expect(add('')).toBe(0)
+})
+
+test('returns the number itself for a single number', () => {
+    expect(add('1')).toBe(1);
+})
+
+test('returns the sum of two numbers', () => {
+    expect(add('1,5')).toBe(6);
+});
