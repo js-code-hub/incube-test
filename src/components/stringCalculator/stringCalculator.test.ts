135aeccdebde5137989836277a6f879c73320baa
Gowthemrazz <er.gowthemrazz@gmail.com>
2024-06-28 20:00:32 +0530

    case1: return 0 for an empty string - passed, case2: returns the number itself for a single number - passed, case3: returns the sum of 2 numbers - passed

--- src/components/stringCalculator/stringCalculator.test.ts
+++ src/components/stringCalculator/stringCalculator.test.ts
@@ -11,3 +11,7 @@ test('returns the number itself for a single number', () => {
 test('returns the sum of two numbers', () => {
     expect(add('1,5')).toBe(6);
 });
+
+test('handles new lines between numbers', () => {
+    expect(add('1\n2, 3')).toBe(6)
+})
